b2a0fa028a2324edad6a657293fbf3d2f41add2a
Cristhian Jimenez <cristhian.jimenez@iatec.com>
2020-04-15 12:48:16 -0400

    Addind files

---
+++ Scripts/Create_Table_Person.sql
@@ -0,0 +1,11 @@
+--create table Person 
+--(
+--	Id int IDENTITY(1,1) PRIMARY KEY,
+--	Name nvarchar(100),
+--	LastName nvarchar(100),
+--	Age int
+--)
+
+--alter table person add constraint FK_person primary key (Id);
+
+select * from Person
